dddab9e102797bc9019cb127d7f2219f12787a95
Ali Hatem <ali.hatem.tahssin@gmail.com>
2025-11-20 00:50:46 +0200

    second commit

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -9,31 +9,28 @@ export default function Sidebar() {
 
   return (
     <div className={`sidebar ${isOpen ? "open" : "closed"}`}>
-        <i class="bi bi-justify" onClick={toggleSidebar}/>
-      {isOpen && (
-        <div id="sidebar">
-          <a>
-            <i class="bi bi-house-door-fill" />
-            <p>Home</p>
-          </a>
-          <a>
-            <i class="bi bi-graph-up-arrow" />
-            <p>Popular</p>
-          </a>
-          <a>
-            <i class="bi bi-bounding-box-circles" />
-            <p>All</p>
-          </a>
-          <a>
-            <i class="bi bi-browser-safari" />
-            <p>Explore</p>
-          </a>
-          <a>
-            <i class="bi bi-bar-chart-line-fill" />
-            <p>Create a Community</p>
-          </a>
-        </div>
-      )}
+      <div id="sidebar">
+        <a>
+          <i class="bi bi-house-door-fill" />
+          <p>Home</p>
+        </a>
+        <a>
+          <i class="bi bi-graph-up-arrow" />
+          <p>Popular</p>
+        </a>
+        <a>
+          <i class="bi bi-bounding-box-circles" />
+          <p>All</p>
+        </a>
+        <a>
+          <i class="bi bi-browser-safari" />
+          <p>Explore</p>
+        </a>
+        <a>
+          <i class="bi bi-bar-chart-line-fill" />
+          <p>Create a Community</p>
+        </a>
+      </div>
     </div>
   );
 }
