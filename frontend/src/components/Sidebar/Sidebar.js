64c7f7f61d7c6830e04001620cc57dc21896c132
Ali Hatem <ali.hatem.tahssin@gmail.com>
2025-11-20 01:24:54 +0200

    third commit

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -19,16 +19,17 @@ export default function Sidebar() {
           <p>Popular</p>
         </a>
         <a>
-          <i class="bi bi-bounding-box-circles" />
+           <i class="bi bi-browser-safari" />
+          <p>Explore</p>
+        
+        </a>
+        <a>
+           <i class="bi bi-bounding-box-circles" />
           <p>All</p>
         </a>
         <a>
-          <i class="bi bi-browser-safari" />
-          <p>Explore</p>
-        </a>
-        <a>
           <i class="bi bi-bar-chart-line-fill" />
-          <p>Create a Community</p>
+          <p>Start a Community</p>
         </a>
       </div>
     </div>
