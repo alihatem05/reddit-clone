81edfcc30ca22a6afd8c025aaf0d3500cf491a01
Ali Hatem <ali.hatem.tahssin@gmail.com>
2025-12-10 21:09:52 +0200

    server fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
@@ -10,10 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5005;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
